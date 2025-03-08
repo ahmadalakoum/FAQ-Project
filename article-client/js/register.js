d8d4534878129f3cd1758adb0a4a3184d1cf7aa9
Ahmad Al Akoum <aakoum02@gmail.com>
2025-03-08 23:25:24 +0200

    Implemented the register page with its style and functionality

--- article-client/js/register.js
+++ article-client/js/register.js
@@ -1,22 +1,21 @@
-document.getElementById("loginForm").addEventListener("submit",async (e)=>{
+document.getElementById("registerForm").addEventListener("submit",async (e)=>{
     e.preventDefault();
-
+    const username =document.getElementById("username").value.trim();
     const email = document.getElementById("email").value.trim();
     const password = document.getElementById("password").value.trim();
     const message =document.getElementById("error-message");
     message.textContent='';
-    if(!email || !password){
+    if(!username || !email || !password){
         message.textContent="All fields are required";
         return;
     }
-    const loginUrl = `${config.apiBaseUrl}/login.php`;
-    const response = await axios.post(loginUrl,{
+    const registerUrl = `${config.apiBaseUrl}/signup.php`;
+    const response = await axios.post(registerUrl,{
+        username,
         email,
         password
     });
-
     if(response.data.status ==="success"){
-        localStorage.setItem("userID",response.data.id);
         window.location.href="home.html";
     }else{
         message.textContent=response.data.message;
